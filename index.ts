8cfe488160fad55ff3aab4e5b07ad72b0a220a64
엘풍 <eruhoon@gmail.com>
2023-02-10 23:42:26 +0900

    update index.ts

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { Bot } from './src/bot/Bot.d.ts';
 import { SimpleBot } from './src/bot/SimpleBot.ts';
 import { IrudaApp } from './src/IrudaApp.ts';
 
-const app = new IrudaApp('wss://mycast.xyz:8002');
+const app = new IrudaApp('');
 
 const bots: Bot[] = [new SimpleBot()];
 
